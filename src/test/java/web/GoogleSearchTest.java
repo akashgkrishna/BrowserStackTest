67855c74a7af33297fb3a04f332abb5935ffb4b4
akashkrishna7 <akashkg@testvagrant.com>
2023-10-04 12:03:46 +0530

    added tests and updated readme

--- src/test/java/web/GoogleSearchTest.java
+++ src/test/java/web/GoogleSearchTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class GoogleSearchTest extends BaseTest {
 
-    @Test
+    @Test(groups = {"sanity", "regression"})
     public void openGoogleAndSearch() throws InterruptedException {
         // Arrange
         String expectedTitle = "TestVagrant - Google Search";
@@ -29,6 +29,60 @@ public class GoogleSearchTest extends BaseTest {
         WebElement searchField = driver.findElement(By.id("APjFqb"));
         searchField.sendKeys(searchTerm);
         searchField.sendKeys(Keys.RETURN);
+
+        // Assert
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Title does not match");
+    }
+
+    @Test(groups = {"regression"})
+    public void openGoogleAndSearchWithImage() throws InterruptedException {
+        // Arrange
+        String expectedTitle = "Google Images";
+
+        // Act
+        driver.get("https://www.google.com");
+
+        // switching to iframe
+        WebElement iframeElement = driver.findElement(By.xpath("//iframe[@role='presentation']"));
+        driver.switchTo().frame(iframeElement);
+
+        // Click on Stay signed out button
+        driver.findElement(By.xpath("//button[text()='Stay signed out']")).click();
+
+        // Switching back to Main
+        driver.switchTo().defaultContent();
+
+        // Clicking on Images link
+        driver.findElement(By.xpath("//a[text()='Images']")).click();
+
+        // Assert
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Title does not match");
+    }
+
+    @Test(groups = {"sanity", "regression"})
+    public void openGoogleAndSearchGmail() throws InterruptedException {
+        //Test to fail assertion
+
+        // Arrange
+        String expectedTitle = "Gmail: Private and secure email at no cost | Google Workspace";
+
+        // Act
+        driver.get("https://www.google.com");
+
+        // switching to iframe
+        WebElement iframeElement = driver.findElement(By.xpath("//iframe[@role='presentation']"));
+        driver.switchTo().frame(iframeElement);
+
+        // Click on Stay signed out button
+        driver.findElement(By.xpath("//button[text()='Stay signed out']")).click();
+
+        // Switching back to Main
+        driver.switchTo().defaultContent();
+
+        /// Clicking on Images link
+        driver.findElement(By.xpath("//a[text()='Gmail']")).click();
 
         // Assert
         String actualTitle = driver.getTitle();
